d3dd73dff8576529c9cbeee83171f9c95012be9e
PanZaifeng <panzf1999@gmail.com>
2024-02-19 18:51:14 +0800

    check pre-commit

--- _plugins/details.rb
+++ _plugins/details.rb
@@ -3,12 +3,12 @@
 module Jekyll
     module Tags
       class DetailsTag < Liquid::Block
-  
+
         def initialize(tag_name, markup, tokens)
           super
           @caption = markup
         end
-  
+
         def render(context)
           site = context.registers[:site]
           converter = site.find_converter_instance(::Jekyll::Converters::Markdown)
@@ -16,9 +16,9 @@ module Jekyll
           body = converter.convert(super(context))
           "<details><summary>#{caption}</summary>#{body}</details>"
         end
-  
+
       end
     end
   end
-  
+
   Liquid::Template.register_tag('details', Jekyll::Tags::DetailsTag)
